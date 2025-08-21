7d67dd3237f03d8b40a4958741d833439e1f2c54
maickel2531 <maickel.sabasgil@iefedericoozanam.edu.co>
2025-08-21 14:54:46 -0500

    5 crud
    fin

--- Casamobler/admin.py
+++ Casamobler/admin.py
@@ -4,3 +4,8 @@ from .models import Usuario, Rol, Cliente, Pedido, Factura, Garantia, ReciboCaja
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Rol)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
+admin.site.register(Factura)
+admin.site.register(Garantia)
+admin.site.register(ReciboCaja)
--- Casamobler/views.py
+++ Casamobler/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Usuario, Rol
+from .models import Usuario, Rol, Cliente, Pedido ,ReciboCaja , Garantia
 
 # Create your views here.
 def index(request):
@@ -46,4 +46,203 @@ def editar_usuarios(request, id):
         return redirect('lista_usuarios')
     return render(request, 'editar_usuarios.html', {'usuario': usuario, 'roles': roles})
 
+def crear_cliente(request):
+    if request.method == 'POST':
+        nombre_cliente = request.POST.get('nombre_cliente')
+        documento_id = request.POST.get('documento_id')
+        direccion = request.POST.get('direccion')
+        telefono = request.POST.get('telefono')
+        correo_electronico = request.POST.get('correo_electronico')
+        Cliente.objects.create(
+            nombre_cliente=nombre_cliente,
+            documento_id=documento_id,
+            direccion=direccion,
+            telefono=telefono,
+            correo_electronico=correo_electronico
+        )
+    return render(request, 'crear_cliente.html')
+
+def lista_cliente(request):
+    return render(request, 'lista_cliente.html', {'clientes': Cliente.objects.all()})
+
+
+def editar_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+    if request.method == 'POST':
+        nombre_cliente = request.POST.get('nombre_cliente')
+        documento_id = request.POST.get('documento_id')
+        direccion = request.POST.get('direccion')
+        telefono = request.POST.get('telefono')
+        correo_electronico = request.POST.get('correo_electronico')
+        cliente.nombre_cliente = nombre_cliente
+        cliente.documento_id = documento_id
+        cliente.direccion = direccion
+        cliente.telefono = telefono
+        cliente.correo_electronico = correo_electronico
+        cliente.save()
+        return redirect('lista_cliente')
+    return render(request, 'editar_cliente.html', {'cliente': cliente})
+
+def eliminar_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+    cliente.delete()
+    return redirect('lista_cliente')
+
+def lista_pedido(request):
+    return render(request, 'lista_pedido.html', {'pedidos': Pedido.objects.all()})
+
+def crear_pedido(request):
+    if request.method == 'POST':
+        fecha_pedido = request.POST.get('fecha_pedido')
+        producto = request.POST.get('producto')
+        cantidad = request.POST.get('cantidad')
+        valor_unitario = request.POST.get('valor_unitario')
+        valor_total = request.POST.get('valor_total')
+        estado_pedido = request.POST.get('estado_pedido')
+        cliente = Cliente.objects.get(id=request.POST.get('cliente'))
+        pedido = Pedido.objects.create(
+            fecha_pedido=fecha_pedido,
+            producto=producto,
+            cantidad=cantidad,
+            valor_unitario=valor_unitario,
+            valor_total=valor_total,
+            estado_pedido=estado_pedido,
+            cliente=cliente
+        )
+        return redirect('lista_pedido')
+    return render(request, 'crear_pedido.html', {'clientes': Cliente.objects.all()})
+
+def editar_pedido(request, id):
+    pedido = get_object_or_404(Pedido, id=id)
+    if request.method == 'POST':
+        fecha_pedido = request.POST.get('fecha_pedido')
+        producto = request.POST.get('producto')
+        cantidad = request.POST.get('cantidad')
+        valor_unitario = request.POST.get('valor_unitario')
+        valor_total = request.POST.get('valor_total')
+        estado_pedido = request.POST.get('estado_pedido')
+        cliente = Cliente.objects.get(id=request.POST.get('cliente'))
+        pedido.fecha_pedido = fecha_pedido
+        pedido.producto = producto
+        pedido.cantidad = cantidad
+        pedido.valor_unitario = valor_unitario
+        pedido.valor_total = valor_total
+        pedido.estado_pedido = estado_pedido
+        pedido.cliente = cliente
+        pedido.save()
+        return redirect('lista_pedido')
+    return render(request, 'editar_pedido.html', {'pedido': pedido, 'clientes': Cliente.objects.all()})
+
+def eliminar_pedido(request, id):
+    pedido = get_object_or_404(Pedido, id=id)
+    pedido.delete()
+    return redirect('lista_pedido')
+
+def lista_recibo_de_caja(request):
+    return render(request, 'lista_recibo_de_caja.html', {'recibos': ReciboCaja.objects.all()})
+
+def crear_recibo_de_caja(request):
+    if request.method == "POST":
+        pedido_id = request.POST.get("pedido")
+        fecha = request.POST.get("fecha_recibo")
+        valor = request.POST.get("valor_abonado")
+        forma_pago = request.POST.get("forma_pago")
+        observaciones = request.POST.get("observaciones")
+
+        pedido = Pedido.objects.get(id=pedido_id)
+
+        ReciboCaja.objects.create(
+            pedido=pedido,
+            fecha_recibo=fecha,
+            valor_abonado=valor,
+            forma_pago=forma_pago,
+            observaciones=observaciones
+        )
+
+        return redirect("lista_recibo_de_caja")
+
+    return render(request, "crear_recibo_de_caja.html", {
+        "pedidos": Pedido.objects.all()})
+
+def editar_recibo_de_caja(request, id):
+    recibo = ReciboCaja.objects.get(id=id)
+
+    if request.method == "POST":
+        recibo.fecha_recibo = request.POST.get("fecha_recibo")
+        recibo.valor_abonado = request.POST.get("valor_abonado")
+        recibo.forma_pago = request.POST.get("forma_pago")
+        recibo.observaciones = request.POST.get("observaciones")
+
+        pedido_id = request.POST.get("pedido")
+        recibo.pedido = Pedido.objects.get(id=pedido_id)
+
+        recibo.save()
+        return redirect("lista_recibo_de_caja")
+
+    return render(request, "editar_recibo_de_caja.html", {
+        "recibo": recibo,
+        "pedidos": Pedido.objects.all()})
+
+def eliminar_recibo_de_caja(request, id):
+    recibo = get_object_or_404(ReciboCaja, id=id)
+    recibo.delete()
+    return redirect('lista_recibo_de_caja')
+
+def lista_garantia(request):
+    return render(request, 'lista_garantia.html', {'garantias': Garantia.objects.all()})
+
+def crear_garantia(request):
+    if request.method == 'POST':
+        producto = request.POST.get('producto')
+        motivo_reclamo = request.POST.get('motivo_reclamo')
+        fecha_reclamo = request.POST.get('fecha_reclamo')
+        estado_reclamo = request.POST.get('estado_reclamo')
+        observaciones = request.POST.get('observaciones')
+        cliente_id = request.POST.get('cliente_id')
+        pedido_id = request.POST.get('pedido_id')
+
+        cliente = Cliente.objects.get(id=cliente_id)
+        pedido = Pedido.objects.get(id=pedido_id)
+
+        Garantia.objects.create(
+            producto=producto,
+            motivo_reclamo=motivo_reclamo,
+            fecha_reclamo=fecha_reclamo,
+            estado_reclamo=estado_reclamo,
+            observaciones=observaciones,
+            cliente=cliente,
+            pedido=pedido
+        )
+    return render(request, 'crear_garantia.html')
+
+def editar_garantia(request, id):
+    garantia = get_object_or_404(Garantia, id=id)
+    if request.method == 'POST':
+        producto = request.POST.get('producto')
+        motivo_reclamo = request.POST.get('motivo_reclamo')
+        fecha_reclamo = request.POST.get('fecha_reclamo')
+        estado_reclamo = request.POST.get('estado_reclamo')
+        observaciones = request.POST.get('observaciones')
+        cliente_id = request.POST.get('cliente_id')
+        pedido_id = request.POST.get('pedido_id')
+
+        cliente = Cliente.objects.get(id=cliente_id)
+        pedido = Pedido.objects.get(id=pedido_id)
+
+        garantia.producto = producto
+        garantia.motivo_reclamo = motivo_reclamo
+        garantia.fecha_reclamo = fecha_reclamo
+        garantia.estado_reclamo = estado_reclamo
+        garantia.observaciones = observaciones
+        garantia.cliente = cliente
+        garantia.pedido = pedido
+        garantia.save()
+        return redirect('lista_garantia')
+    return render(request, 'editar_garantia.html', {'garantia': garantia})
+
+def eliminar_garantia(request, id):
+    garantia = get_object_or_404(Garantia, id=id)
+    garantia.delete()
+    return redirect('lista_garantia')
+    
 
